830e9f3952c0ca3b87c7521343c691b7a9a52943
monis8khan@gmail.com <monis8khan@gmail.com>
2022-07-27 16:58:15 +0500

    CTA and About section done

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -2,12 +2,12 @@ import { Button } from "./common/Button"
 
 export const CTA = ({title, desc, btnText}) => {
   return (
-    <section id="CTA" className="w-full bg-skin-button-accent items-center p-10 px-20">
+    <section id="CTA" className="w-full bg-skin-button-accent items-center py-10 md:px-20">
         <div className="container mx-auto flex flex-col items-center text-center">
             <div className="w-2/3">
-                <h2 className="text-skin-base text-4xl text-underline mb-3 font-bold">{title}
+                <h2 className="text-skin-base text-lg md:text-4xl text-underline mb-3 font-bold">{title}
                 </h2>
-                <p className="leading-relaxed text-skin-base p-2">{desc}
+                <p className="leading-relaxed text-skin-base text-xs md:text-lg p-2">{desc}
                 </p>
             </div>
             <div className="w-1/3 flex flex-row justify-center">                    
