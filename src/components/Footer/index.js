506fd1d6c6693f96a88523307e9891131714f8ea
monis8khan@gmail.com <monis8khan@gmail.com>
2022-09-27 09:52:36 +0500

    updated the sections and footer as well..

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import AnimatedLetters from '../common/AnimatedLetters';
 import { SocialIcons } from '../common/SocialIcons'
 
 const Footer = ({ icons }) => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass('text-animate-hover')
+        }, 2000)
+    }, [])
     return (
         <footer className="">
             <div className="container mx-auto flex flex-col-reverse md:flex-row text-center md:text-left">
                 <div className="flex flex-col md:w-2/3 p-10">
-                    <h2 className="text-skin-base text-3xl font-semibold hover:text-skin-muted">
-                        <a href="#Home">Monis8Khan.github.io</a>
+                    <h2 className="text-skin-base text-3xl font-semibold">
+                        <a href="#Monis8Khan">
+                            <AnimatedLetters strArray={"Monis8Khan.github.io".split("")} idx={1} lettersClass={letterClass}> </AnimatedLetters>
+                            </a>
                     </h2>
                     {/* <p className="text-skin-base my-auto">I am Javascript developer with great skills in customization of wix/editorX websites using Velo-Api. 
                     I can integrate 3rd party applications in your wix website using rest APIs. 
                     I can create custom functionality using wix-collections and velo code.</p> */}
-                    <p className="text-sm text-skin-base pt-4 font-bold">Usefull Links
+                    <p className="text-sm text-skin-base pt-4 font-bold">
+                            <AnimatedLetters strArray={"Usefull Links".split("")} idx={1} lettersClass={letterClass}> </AnimatedLetters>
                     </p>
                     <div className="flex flex-row justify-center md:justify-start py-2 text-xs">
                         <a href="#About">
@@ -31,7 +41,9 @@ const Footer = ({ icons }) => {
                             </li>
                         </a>
                     </div>
-                    <p className="text-xs md:text-sm text-skin-muted mt-10 border-t-2 pt-5">© 2022 MONIS8KHAN All Rights Reserved
+                    <p className="text-xs md:text-sm text-skin-base mt-10 border-t-2 pt-5">
+                        
+                    <AnimatedLetters strArray={"© 2022 MONIS8KHAN All Rights Reserved".split("")} idx={1} lettersClass={letterClass}> </AnimatedLetters>
                     </p>
 
                 </div>
@@ -43,6 +55,7 @@ const Footer = ({ icons }) => {
                         {icons.map((icon) => (<SocialIcons key={icon.id} icon={icon.svg} link={icon.url} classes='fill-red-800 ml-4 first:ml-0' />))}
                     </div>
                     <p className="text-xs md:text-sm text-skin-base text-center md:text-right">Developer | JavaScript | TailwindCSS | Angular | Typescript | Linux
+                    {/* <AnimatedLetters strArray={"© 2022 MONIS8KHAN All Rights Reserved".split("")} idx={1} lettersClass={letterClass}> </AnimatedLetters> */}
                     </p>
                 </div>
             </div>
--- src/components/common/ServiceCard.js
+++ src/components/common/ServiceCard.js
@@ -1,12 +1,23 @@
-export const ServiceCard = ({name, description, image, id}) => {
+import { useState, useEffect } from "react";
+import AnimatedLetters from "./AnimatedLetters";
+
+export const ServiceCard = ({ name, description, image, id }) => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass('text-animate-hover')
+        }, 2000)
+    }, [])
     return (
         <div
-            className={id%2 ===0 ? "flex items-center container mx-auto py-10 md:px-20 mb-10 sm:flex-row flex-col hover:-translate-y-1 border-t-2 border-inherit" : 
-            "flex items-center container mx-auto py-10 md:px-20 mb-10 sm:flex-row-reverse flex-col hover:-translate-y-1 border-t-2 border-inherit"}>
+            className={id % 2 === 0 ? "flex items-center container mx-auto py-10 md:px-20 mb-10 sm:flex-row flex-col hover:-translate-y-1 border-t-2 border-inherit" :
+                "flex items-center container mx-auto py-10 md:px-20 mb-10 sm:flex-row-reverse flex-col hover:-translate-y-1 border-t-2 border-inherit"}>
             <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-                <h2 className="text-skin-base text-base md:text-2xl text-underline mb-3 font-bold underline">{name}
+                <h2 className="text-skin-base text-base md:text-2xl text-underline mb-3 font-bold underline">
+                    <AnimatedLetters strArray={name.split("")} idx={1} lettersClass={letterClass}> </AnimatedLetters>
                 </h2>
-                <p className="text-skin-base text-xs md:text-base">{description}
+                <p className="text-skin-base text-xs md:text-base">
+                    <AnimatedLetters strArray={description.split("")} idx={1} lettersClass={letterClass}> </AnimatedLetters>
                 </p>
                 <a href="#Portfolio"
                     className="mt-3 text-skin-inverted inline-flex items-center hover:translate-x-1 hover:text-skin-base hover:px-3 hover:rounded-full">Hire
@@ -18,8 +29,8 @@ export const ServiceCard = ({name, description, image, id}) => {
                 </a>
             </div>
             <div
-                className={id%2 ===0 ?  "sm:w-32 sm:order-none order-first sm:h-32 h-20 w-20 inline-flex items-center justify-center rounded-full bg-white flex-shrink-0 sm:ml-10" : 
-                "sm:w-32 sm:order-none order-first sm:h-32 h-20 w-20 inline-flex items-center justify-center rounded-full bg-white flex-shrink-0 sm:mr-10"}>
+                className={id % 2 === 0 ? "sm:w-32 sm:order-none order-first sm:h-32 h-20 w-20 inline-flex items-center justify-center rounded-full bg-white flex-shrink-0 sm:ml-10" :
+                    "sm:w-32 sm:order-none order-first sm:h-32 h-20 w-20 inline-flex items-center justify-center rounded-full bg-white flex-shrink-0 sm:mr-10"}>
                 <img src={image} alt="website" className="h-10 text-skin-base" />
             </div>
         </div>)
